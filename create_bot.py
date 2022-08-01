a773b147282542e052edfb6fe3e1f04acc876eeb
ilnurgabdullin <ilnurgabdullin627@gmail.com>
2022-08-01 14:15:11 +0300

    first_commit

--- create_bot.py
+++ create_bot.py
@@ -5,7 +5,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 
 
-bot = Bot(token = '5347235715:AAEHEFFLyjPoH-WNCJnhVZxJFf1PuJAcrxw')
+bot = Bot(token = '5347235715:AAEHEFFLyjPoH-WNCJnhVZxJFf1PuJAcrxvv')
 dp = Dispatcher(bot, storage= storage)
 
 
